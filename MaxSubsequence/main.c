a7005546c45d21207ee9df080b8fbe5ea573f1de
qingsong <qs_hero@163.com>
2018-07-02 10:30:31 +0800

    优化最大序列之和算法

--- MaxSubsequence/main.c
+++ MaxSubsequence/main.c
@@ -8,7 +8,8 @@
 
 #include <stdio.h>
 
-int maxSubsequenceSum(const int A[], int N) {
+/// 最大序列方法一：
+int MaxSubsequenceSum_1(const int A[], int N) {
     int maxSum, thisSum, i;
     maxSum = thisSum = 0;
     for (i = 0; i < N; i++) {
@@ -49,6 +50,8 @@ static int MaxSubSum(const int A[], int left, int right) {
     int leftBorderSum, rightBorderSum;
     int center, i;
     
+    int max_left = 0, max_right = 0;
+    
     if (left == right) {
         if (A[left] > 0) {
             return A[left];
@@ -62,40 +65,60 @@ static int MaxSubSum(const int A[], int left, int right) {
     maxRightSum = MaxSubSum(A, center + 1, right);
     
     maxLeftBorderSum = 0; leftBorderSum = 0;
-    for (i = center; i >= left; i--) {
+    for (i = center; i >= left; i--)
+    {
         leftBorderSum += A[i];
         if (leftBorderSum > maxLeftBorderSum) {
             maxLeftBorderSum = leftBorderSum;
+            max_left = i;
         }
     }
     
     maxRightBorderSum = 0; rightBorderSum = 0;
-    for (i = center; i >= left; i--) {
+    for (i = center + 1; i <= right; i++)
+    {
         rightBorderSum += A[i];
-        if (leftBorderSum > maxRightBorderSum) {
+        if (rightBorderSum > maxRightBorderSum) {
             maxRightBorderSum = rightBorderSum;
+            max_right = i;
         }
     }
     
+    if (maxLeftSum > maxRightSum && maxLeftSum > maxLeftBorderSum + maxRightBorderSum) {
+        printf(" 0 max = %d index left = %d right = %d\n",maxLeftSum, max_left, max_right);
+        return maxLeftSum;
+    }
+    if (maxLeftSum < maxRightSum && maxRightSum > maxLeftBorderSum + maxRightBorderSum) {
+        printf(" 1 max = %d index left = %d right = %d\n",maxRightSum, max_left, max_right);
+        return maxRightSum;
+    }
+    printf("2 max = %d index left = %d right = %d\n",maxLeftBorderSum + maxRightBorderSum, max_left, max_right);
+
+    return maxLeftBorderSum + maxRightBorderSum;
     return MaxNumber(maxLeftSum, maxRightSum, maxLeftBorderSum + maxRightBorderSum);
 }
 
-int MaxSubsequenceSum(const int A[], int N)
+/// 最大序列之和 方法二
+int MaxSubsequenceSum_2(const int A[], int N)
 {
     return MaxSubSum(A, 0, N - 1);
 }
 
 
-
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
     
     int A[] = {4, -3, 5, -2, -1, 2, 6, 2};
     int num = sizeof(A) / sizeof(A[0]);
     
-    int maxSum = maxSubsequenceSum(A, num);
-    printf("max sum is %d\n", maxSum);
+    // 计算最大序列方法1：
+    int maxSum = MaxSubsequenceSum_1(A, num);
+    printf("方法一：max sum is %d\n", maxSum);
+
+    // 计算最大序列方法2：
+    int maxSum2 = MaxSubsequenceSum_2(A, num);
+    
+    printf("方法二：max sum is %d\n", maxSum2);
     
     return 0;
 }
